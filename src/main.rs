19c937dd19251d9e2bc0bb67319766ea48e2396d
DawnMagnet <axccjqh@qq.com>
2025-04-12 16:27:18 +0800

    add options

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use clap::{Parser, Subcommand};
 use std::io::{self, Read, Write};
-use substitution_cipher_solver::ciphey::{decrypt, encrypt, get_input, get_output};
+use substitution_cipher_solver::ciphey::{
+    decrypt, encrypt, get_input, get_output, set_max_bad_words_rate, set_max_goodness_level,
+};
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
@@ -13,21 +15,27 @@ enum Commands {
     /// Decrypts the input file
     Decrypt {
         /// Path to the encrypted file
-        #[clap(short, long)]
+        #[clap(value_parser)]
         input: Option<String>,
 
         /// Path to the decrypted file
-        #[clap(short, long)]
+        #[clap(value_parser)]
         output: Option<String>,
+
+        #[arg(long, short, default_value_t = 0.06)]
+        badword_percent: f32,
+
+        #[arg(long, short, default_value_t = 2)]
+        goodness_level: usize,
     },
     /// Encrypts the input file
     Encrypt {
         /// Path to the encrypted file
-        #[clap(short, long)]
+        #[clap(value_parser)]
         input: Option<String>,
 
         /// Path to the decrypted file
-        #[clap(short, long)]
+        #[clap(value_parser)]
         output: Option<String>,
     },
 }
@@ -37,9 +45,19 @@ fn main() -> io::Result<()> {
     let cli = Cli::parse();
 
     match &cli.command {
-        Commands::Decrypt { input, output } => {
+        Commands::Decrypt {
+            input,
+            output,
+            badword_percent,
+            goodness_level,
+        } => {
             let mut input_file: Box<dyn Read> = get_input(input);
             let mut output_file: Box<dyn Write> = get_output(output);
+            let badword_rate = (badword_percent * 100.0) as usize;
+            let goodness_level = *goodness_level;
+            set_max_bad_words_rate(badword_rate);
+            set_max_goodness_level(goodness_level);
+
             decrypt(&mut input_file, &mut output_file)?;
         }
         Commands::Encrypt { input, output } => {
